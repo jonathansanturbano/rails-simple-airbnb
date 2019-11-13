fed50a686744ac1133ee954c27fcff53afb898a9
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2019-11-13 16:51:52 +0100

    index started needs styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Amazing View over Paris',
+  address: '22 rue des Abbesses',
+  description: 'It has no windows',
+  price_per_night: 850,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Mi casa es su casa',
+  address: '12 Plaza Mayor',
+  description: 'Tapas all day all night',
+  price_per_night: 9,
+  number_of_guests: 12
+)
+
+Flat.create!(
+  name: 'Gutentag',
+  address: '4 Furgen',
+  description: 'Mein Freund',
+  price_per_night: 57,
+  number_of_guests: 3
+)
